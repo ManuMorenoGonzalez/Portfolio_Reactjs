7810fb160b1631f53e317c3e6fdb506baa97cbaf
ManuMorenoGonzalez <manuelmorenodeveloper@gmail.com>
2022-11-02 23:42:56 +0100

    First commit to React Typescript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Próximamente... Libreto de Carnaval
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
